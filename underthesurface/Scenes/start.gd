5e59863e0a012b8b3002e9604c1702c1c2687665
Williamnumismatics <williamnumismatics@gmail.com>
2026-02-28 17:03:03 +1100

    added animations

--- underthesurface/Scenes/start.gd
+++ underthesurface/Scenes/start.gd
@@ -2,4 +2,6 @@ extends Control
 
 
 func _on_button_continue_pressed() -> void:
+	$AnimationPlayer.play("fade_out")
+	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn") # Replace with function body.
